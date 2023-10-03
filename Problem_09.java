0213e6f7e3b082f669b671ba5febceac4dd43018
S <s>
2023-10-03 22:28:59 +0400

    {"types":"C","runs":0}

--- Problem_09.java
+++ Problem_09.java
@@ -4,4 +4,19 @@ public class Problem_09 extends SuperKarel {
 	public void run() {
 		
 	}
+	
+	private void multiply() {
+		turnLeft();
+		move();
+		while (beepersPresent()) {
+			move();
+			while (beepersPresent()) {
+				pickBeeper();
+				move();
+				putBeeper();
+				move();
+				putBeeper();
+			}
+		}
+	}
 }
