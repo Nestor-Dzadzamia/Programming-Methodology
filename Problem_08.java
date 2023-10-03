8b1609289953b0f8dbb49df3c2458eec431f262e
S <s>
2023-10-03 23:33:04 +0400

    {"types":"C","runs":24}

--- Problem_08.java
+++ Problem_08.java
@@ -1,4 +1,34 @@
+import stanford.karel.SuperKarel;
 
-public class Problem_08 {
-
+public class Problem_08 extends SuperKarel {
+	public void run() {
+		turnLeft();
+		move();
+		move();
+		subtract();
+	}
+	
+	private void subtract() {
+		while (beepersPresent()) {
+			pickBeeper();
+			turnAround();
+			move();
+			pickBeeper();
+			turnAround();
+			move();
+		}
+		turnAround();
+		move();
+		turnAround();
+		while (beepersPresent()) {
+			pickBeeper();
+			move();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+			turnAround();
+		}
+	}
 }
