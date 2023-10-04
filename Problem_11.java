8601d1478d5e0b3e60b82b895e1795dcf202d35e
S <s>
2023-10-05 00:14:52 +0400

    {"types":"C","runs":0}

--- Problem_11.java
+++ Problem_11.java
@@ -2,27 +2,6 @@ import stanford.karel.SuperKarel;
 
 public class Problem_11 extends SuperKarel {
 	public void run() {
-		while (frontIsClear()) {
-			if (beepersPresent()) {
-				stop();
-			}
-			move();
-			if (frontIsBlocked()) {
-				findAWay();
-			}
-		}
+		
 	}
-	
-	private void findAWay() {
-		if (leftIsClear()) {
-			turnLeft();
-		} else {
-			if (rightIsClear()) {
-				turnRight();
-			} else {
-				turnAround();
-			}
-		}
-	}
-	
 }
