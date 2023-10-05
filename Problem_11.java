9dae930b88a82b7ed92fc7a3560209e91da119db
S <s>
2023-10-05 21:39:22 +0400

    {"types":"C","runs":0}

--- Problem_11.java
+++ Problem_11.java
@@ -2,6 +2,11 @@ import stanford.karel.SuperKarel;
 
 public class Problem_11 {
 	public void run() {
-		
+		int a = 10;
+		int b = 15;
+		a = a^b;
+		b = a^b;
+		a = a^b;
+		System.out.print(a + " " + b);
 	}
 }
