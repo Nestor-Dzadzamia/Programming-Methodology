c53238870bd0c861483f4fcfe0fb9a8cb9d3b730
S <s>
2023-10-05 21:43:44 +0400

    {"types":"C","runs":4}

--- Problem_11.java
+++ Problem_11.java
@@ -1,12 +1,7 @@
 import stanford.karel.SuperKarel;
 
-public class Problem_11 {
+public class Problem_11 extends SuperKarel {
 	public void run() {
-		int a = 10;
-		int b = 15;
-		a = a^b;
-		b = a^b;
-		a = a^b;
-		System.out.print(a + " " + b);
+		
 	}
 }
