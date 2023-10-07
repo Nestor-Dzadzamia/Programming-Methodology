c1ad955c2a35f68a85fa956a74063a16cd12858e
S <s>
2023-10-07 23:57:41 +0400

    {"types":"C","runs":2}

--- Practice_Class.java
+++ Practice_Class.java
@@ -1,4 +1,55 @@
+import stanford.karel.SuperKarel;
 
-public class Practice_Class {
+public class Practice_Class extends SuperKarel{
+	public void run() {
+		moveFirst();
+		pickBeepers();
+		putBeepers();
+	}
+
+	private void putBeepers() {
+		turnAround();
+		while (frontIsClear()) {
+			move();
+		}
+		turnRight();
+		move();
+		while (beepersPresent()) {
+			move();
+		}
+		while (beepersInBag()) {
+			putBeeper();
+		}
+		
+	}
+
+	private void moveFirst() {
+		move();
+		while (beepersPresent()) {
+			pickBeeper();
+		}
+		turnAround();
+		move();
+		turnRight();
+		move();
+		while (beepersInBag()) {
+			putBeeper();
+		}
+		turnAround();
+		move();
+		turnLeft();
+		move();
+	}
+
+	private void pickBeepers() {
+		while (noBeepersPresent()) {
+			move();
+		}
+		
+		while (beepersPresent()) {
+			pickBeeper();
+		}
+	}
+
 
 }
