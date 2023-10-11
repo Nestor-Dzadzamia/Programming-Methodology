820026e808fb373be84062eb04f28eaad90409cc
S <s>
2023-10-12 00:33:59 +0400

    {"types":"C","runs":36}

--- Problem14.java
+++ Problem14.java
@@ -12,8 +12,8 @@ public class Problem14 extends GraphicsProgram {
 
 	private void drawTheSybol() {
 		GLine line1 = new GLine(100, 100, 130, 140);
-		GLine line2 = new GLine(100, 100, 140, 70);
-		GLine line3 = new GLine(130, 140, 170, 110);
+		GLine line2 = new GLine(100, 100, 130, 60);
+		GLine line3 = new GLine(130, 140, 160, 100);
 		GLine line4 = new GLine(170, 110, 200, 150);
 		line1.setColor(Color.black);
 		add(line1);
