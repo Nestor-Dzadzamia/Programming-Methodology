00bfe9fec6a931b16b3119246270a749f66c9a85
S <s>
2023-10-11 23:58:41 +0400

    {"types":"C","runs":20}

--- Problem14.java
+++ Problem14.java
@@ -18,8 +18,8 @@ public class Problem14 extends GraphicsProgram {
 	private void drawNose() {
 		int height  = getHeight();
 		int width = getWidth();
-		int x = 50;
-		int y = 40;
+		int x = 30;
+		int y = 30;
 		
 		GRect nose = new GRect(x, y);
 		nose.setFilled(true);
