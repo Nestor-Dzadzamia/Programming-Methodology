60f0c9098d4c82409fc587c67b24604b387a16c6
S <s>
2023-10-11 23:27:37 +0400

    {"types":"C","runs":0}

--- Problem14.java
+++ Problem14.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Problem14 extends GraphicsProgram {
@@ -10,6 +13,32 @@ public class Problem14 extends GraphicsProgram {
 		drawEyes();
 		drawMouth();
 		drawNose();
+	}
+
+	private void drawNose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void drawMouth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void drawEyes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void drawFace() {
+		int height = getHeight();
+		int width = getWidth();
+		
+		GRect face = new GRect(300, 300);
+		face.setFilled(true);
+		face.setColor(Color.gray);
+		add(face, (height / 2) - 150, (width / 2) - 150);
+		
 	}
 
 }
