cf0a84876e662ed6e6e324bf593e907602a7bcda
S <s>
2023-10-11 23:24:01 +0400

    {"types":"C","runs":0}

--- Problem14.java
+++ Problem14.java
@@ -1,4 +1,15 @@
+import acm.program.GraphicsProgram;
 
 public class Problem14 extends GraphicsProgram {
+	public void run() {
+		drawRobot();
+	}
+
+	private void drawRobot() {
+		drawFace();
+		drawEyes();
+		drawMouth();
+		drawNose();
+	}
 
 }
