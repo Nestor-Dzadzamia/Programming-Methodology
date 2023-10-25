af918d58ea7019eee9e645eb43450834b82b6726
S <s>
2023-10-25 22:29:26 +0400

    {"types":"C","runs":26}

--- Problem_31.java
+++ Problem_31.java
@@ -10,7 +10,6 @@ public class Problem_31 extends ConsoleProgram{
 		
 		if (n > 1) {
 			int divisors = (int)Math.sqrt(n);
-			int count = 0;
 			for(int i = divisors; i > 1; i--) {
 				if (n % i == 0) {
 					return false;
