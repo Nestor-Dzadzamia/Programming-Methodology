1a492cd38b5816498d2c8eec798368d51696cb2a
S <s>
2023-10-25 22:29:00 +0400

    {"types":"C","runs":26}

--- Problem_32.java
+++ Problem_32.java
@@ -6,6 +6,15 @@ public class Problem_32 extends ConsoleProgram{
 	}
 	
 	private boolean isPrime(int n) {
-		int divisors 
+		int divisors = (int) Math.sqrt(n);
+		
+		if (n > 1) {
+			for (int i = divisors; i > 1; i--) {
+				if (n % i == 0) {
+					return false;
+				}
+			}
+		}
+		
 	}
 }
