eb0fa76813ef8388fe35a38a9f19164be2764e49
S <s>
2023-10-25 22:29:56 +0400

    {"types":"C","runs":26}

--- Problem_32.java
+++ Problem_32.java
@@ -10,9 +10,7 @@ public class Problem_32 extends ConsoleProgram{
 		
 		if (n > 1) {
 			for (int i = divisors; i > 1; i--) {
-				if (n % i == 0) {
-					return false;
-				}
+				if (n % i == 0) return false;
 			}
 		} else return false;
 		
