f768b18ac175ce41023049c104cd4715fd116b03
S <s>
2023-10-25 21:55:56 +0400

    {"types":"C","runs":9}

--- Problem_29.java
+++ Problem_29.java
@@ -4,7 +4,7 @@ public class Problem_29 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number : ");
 		
-		fibonacci(n);
+		println(fibonacci(n));
 	}
 	
 	private int fibonacci(int n) {
