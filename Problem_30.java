6dc8e2b1cf19a85f80bd39b078de3d58292ebfc3
S <s>
2023-10-25 21:39:31 +0400

    {"types":"C","runs":0}

--- Problem_30.java
+++ Problem_30.java
@@ -1,4 +1,34 @@
+import acm.program.ConsoleProgram;
 
-public class Problem_30 {
-
+public class Problem_30 extends ConsoleProgram {
+	public void run() {
+		int number = readInt("Enter your number : ");
+		
+		if (number > 0) {
+			println(reverse(number));
+		} else {
+			println(0 - reverse(number));
+		}
+	}
+	
+	private int reverse(int number) {
+		number = Math.abs(number);
+		int digits = 0;
+		int answ = 0;
+		
+		for (int i = number; i > 0; i /= 10) {
+			digits++;
+		}
+		
+		for (int i = digits; i > 0 ; i--) {
+			while (number > 0) {
+				double newNum = (number % 10) * Math.pow(10, i - 1);
+				number /= 10;
+				i--;
+				answ += newNum;
+			}
+		}
+		
+		return answ;
+	}
 }
