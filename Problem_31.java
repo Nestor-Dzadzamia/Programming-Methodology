9c6d404e92e34417c9100d521267ee748db1aad8
S <s>
2023-10-25 22:22:45 +0400

    {"types":"C","runs":19}

--- Problem_31.java
+++ Problem_31.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Problem_31 extends ConsoleProgram{
 	public void run() {
-		int n = readInt("Enter your number:");
+		int n = readInt("Enter your number : ");
 		println(isPrime(n));
 	}
 	
