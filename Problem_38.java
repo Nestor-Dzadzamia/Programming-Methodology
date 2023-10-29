be87169e3c9512ac5c3c073ab04175af9b954968
S <s>
2023-10-29 18:14:05 +0400

    {"types":"C","runs":101}

--- Problem_38.java
+++ Problem_38.java
@@ -14,12 +14,15 @@ public class Problem_38 extends ConsoleProgram{
 		while (balance != 0) {
 			int bid = readInt("Enter The Amount You Would like to bid : ");
 			int bidNumber = readInt("Enter The Number to bet on : ");
-			
+			balance -= bid;
 			int winNumber = rgen.nextInt(0, 36);
 			
 			println("Winning Number is -- " + winNumber);
 			if (winNumber == bidNumber) {
-		
+				balance += bid * 2;
+				println("Congrats, you won! your new balance : " + balance);
+			} else {
+				println("Unfortunately, you Lost! your new Balance : " + balance);
 			}
 		}
 	}
