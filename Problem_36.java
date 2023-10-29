85a960a876f15297d63277545e1c5d116b61042d
S <s>
2023-10-29 17:23:09 +0400

    {"types":"C","runs":73}

--- Problem_36.java
+++ Problem_36.java
@@ -13,7 +13,15 @@ public class Problem_36 extends ConsoleProgram {
 		for (int i = 0; i < NUMBER_OF_GAMES; i++) {
 			while (true) {
 				int coin = rgen.nextInt(0, 1);
+				if (coin == 1) {
+					countHeads++;
+				} else {
+					countHeads = 0;
+				}
+				
+				if (countHeads == 3) break;
 			}
+			
 		}
 	}
 }
