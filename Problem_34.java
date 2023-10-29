a4949b9fb49c40ed81268e2314304c501e9358d1
S <s>
2023-10-29 15:35:18 +0400

    {"types":"C","runs":23}

--- Problem_34.java
+++ Problem_34.java
@@ -1,8 +1,18 @@
+import java.awt.Color;
+
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class Problem_34	extends GraphicsProgram {
+	private	RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
 		
+	}
+	
+	private void drawRandomCircle() {
+		Color color = rgen.nextColor();
+		int xCoordinate = rgen.nextInt();
+		int yCoordinate = rgen.nextInt();
 	}
 
 }
