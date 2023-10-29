ee2d325ea12f5f82a444909bf250583e90fc7a6b
S <s>
2023-10-29 17:10:54 +0400

    {"types":"C","runs":73}

--- Problem_36.java
+++ Problem_36.java
@@ -1,11 +1,14 @@
 import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
 public class Problem_36 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int NUMBER_OF_GAMES = 100;
 	public void run() {
 		
 	}
 	
-	public void double avgTrialsForThreeHeads() {
-		
+	public double avgTrialsForThreeHeads() {
+	
 	}
 }
