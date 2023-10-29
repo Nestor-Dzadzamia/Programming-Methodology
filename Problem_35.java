a976c4c14ce4a974132ada1b077a8d897f47bd3d
S <s>
2023-10-29 16:23:54 +0400

    {"types":"C","runs":38}

--- Problem_35.java
+++ Problem_35.java
@@ -10,9 +10,9 @@ public class Problem_35 extends ConsoleProgram {
 	
 	// total trials to get observe side of a coin
 	private int totalTrials() {
+		int count = 1;
 		for (int i = 0; i < NUMBER_OF_TRIALS; i++) {
 			int coin = rgen.nextInt(0,1);
-			int count = 1;
 			if (coin == 1) {
 				return count;
 			} else count++;
