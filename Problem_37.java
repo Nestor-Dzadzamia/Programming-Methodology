5022a44413d68395ce0e11c3c5a42d670458bab3
S <s>
2023-10-29 17:45:00 +0400

    {"types":"C","runs":100}

--- Problem_37.java
+++ Problem_37.java
@@ -2,6 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class Problem_37 extends ConsoleProgram{
 	public void run() {
-		
+		playRoulette();
+	}
+	
+	public void playRoulette() {
+		int number = readInt("Enter the number you would like to bid on");
 	}
 }
