0ea87d3dcf916267bfaa0ee8dea6895f833cfff1
S <s>
2023-10-29 16:16:35 +0400

    {"types":"C","runs":38}

--- Problem_35.java
+++ Problem_35.java
@@ -1,7 +1,13 @@
 import acm.program.ConsoleProgram;
 
 public class Problem_35 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int NUMBER_OF_TRIALS = 10;
 	public void run() {
+		
+	}
+	
+	private boolean isObserve() {
 		
 	}
 
