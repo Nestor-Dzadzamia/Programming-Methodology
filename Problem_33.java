fd9ba31131ebb98eb7b398b9cafd904e4050263c
S <s>
2023-10-29 15:21:41 +0400

    {"types":"C","runs":8}

--- Problem_33.java
+++ Problem_33.java
@@ -7,13 +7,13 @@ public class Problem_33 extends GraphicsProgram{
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	public void run() {
-		
 		drawRandomCircle();
-		
 		
 	}
 	
 	private void drawRandomCircle() {
+		int width = getWidth();
+		int height = getHeight();
 		
 	}
 
