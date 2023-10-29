50e7ac879442388006d3f4c9eb498dfd65cf21bc
S <s>
2023-10-29 17:46:26 +0400

    {"types":"C","runs":100}

--- Problem_37.java
+++ Problem_37.java
@@ -1,12 +1,14 @@
 import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Problem_37 extends ConsoleProgram{
+public class Problem_37 extends ConsoleProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
 		playRoulette();
 	}
 	
 	public void playRoulette() {
-		int number = readInt("Enter the number you would like to bet on : ");
-		
+		int number = readInt("Enter the number you would like to bet on --- ");
+		int winNumber = 
 	}
 }
