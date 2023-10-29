cb5ac7857737da463252dcfdd5b89c8fc92d2460
S <s>
2023-10-29 15:19:37 +0400

    {"types":"C","runs":8}

--- Problem_33.java
+++ Problem_33.java
@@ -1,9 +1,12 @@
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class Problem_33 extends GraphicsProgram{
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	public void run() {
+		
+		
 		
 	}
 
