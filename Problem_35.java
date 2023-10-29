404e5b87b60b690e2465cb429dc738869f935f9a
S <s>
2023-10-29 17:06:13 +0400

    {"types":"C","runs":49}

--- Problem_35.java
+++ Problem_35.java
@@ -3,7 +3,7 @@ import acm.util.RandomGenerator;
 
 public class Problem_35 extends ConsoleProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int NUMBER_OF_GAMES = 100;
+	private static final int NUMBER_OF_GAMES = 100000;
 	public void run() {
 		println(avgNumberOfTrials());
 	}
