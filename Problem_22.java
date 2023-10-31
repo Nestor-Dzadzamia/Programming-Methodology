659034ff71ddcc7041443f99386e28f750e14088
S <s>
2023-10-31 22:56:30 +0400

    {"types":"C","runs":0}

--- Problem_22.java
+++ Problem_22.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
@@ -6,11 +8,12 @@ public class Problem_22 extends GraphicsProgram {
 		double width = getWidth();
 		double height = getHeight();
 		
-		for (double i = width / 8; i <= width; i += width / 8) {
-			for (double j = height / 8; j <= height; j += height / 8) {
-				GRect horizontalLine = new GRect(width / 8, height / 8);
-				GRect verticalLine = new GRect(width / 8, height / 8);
-				
+		for (double i = 0; i <= width; i += width / 8) {
+			for (double j = 0; j <= height; j += height / 8) {
+				GRect rect = new GRect(width / 8, height / 8);
+				rect.setFilled(true);
+				rect.setFillColor(Color.cyan);
+				add(rect, i, j);
 			}
 		}
 	}
