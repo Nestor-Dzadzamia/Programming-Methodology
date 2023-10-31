17c28bac8a3cb1eca80a7572467612562f4d22a9
S <s>
2023-10-31 22:55:07 +0400

    {"types":"C","runs":0}

--- Problem_22.java
+++ Problem_22.java
@@ -9,7 +9,7 @@ public class Problem_22 extends GraphicsProgram {
 		for (double i = width / 8; i <= width; i += width / 8) {
 			for (double j = height / 8; j <= height; j += height / 8) {
 				GRect horizontalLine = new GRect(width / 8, height / 8);
-				GRect verticalLine = new GRect(i, 0, i, height);
+				GRect verticalLine = new GRect(width / 8, height / 8);
 				
 			}
 		}
