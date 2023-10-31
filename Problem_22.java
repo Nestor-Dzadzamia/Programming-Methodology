7a804600b0f457cd1afa8f5e5c76fb42dd816cf2
S <s>
2023-10-31 22:59:41 +0400

    {"types":"C","runs":6}

--- Problem_22.java
+++ Problem_22.java
@@ -10,8 +10,8 @@ public class Problem_22 extends GraphicsProgram {
 		double width = getWidth();
 		double height = getHeight();
 		
-		for (double i = 0; i <= 8 * BLOCK_SIZE; i += BLOCK_SIZE) {
-			for (double j = 0; j <= 8 * BLOCK_SIZE; j += BLOCK_SIZE) {
+		for (double i = 0; i < 8 * BLOCK_SIZE; i += BLOCK_SIZE) {
+			for (double j = 0; j < 8 * BLOCK_SIZE; j += BLOCK_SIZE) {
 				GRect rect = new GRect(BLOCK_SIZE, BLOCK_SIZE);
 				rect.setFilled(true);
 				rect.setFillColor(Color.cyan);
