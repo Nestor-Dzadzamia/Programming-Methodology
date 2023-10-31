cf114ed5bf04419abcac84ef72d37a83679dc456
S <s>
2023-10-31 23:01:19 +0400

    {"types":"C","runs":7}

--- Problem_22.java
+++ Problem_22.java
@@ -14,7 +14,7 @@ public class Problem_22 extends GraphicsProgram {
 			for (double j = 0; j < 8 * BLOCK_SIZE; j += BLOCK_SIZE) {
 				GRect rect = new GRect(BLOCK_SIZE, BLOCK_SIZE);
 				rect.setFilled(true);
-				if ()
+				if (j % 2 == 0) rect.setFillColor(Color.white);
 				add(rect, i, j);
 			}
 		}
