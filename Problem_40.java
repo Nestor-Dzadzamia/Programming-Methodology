35f7618b7ce7f9cbeb86ce51146c47db360f40b2
S <s>
2023-11-02 23:36:01 +0400

    {"types":"C","runs":7}

--- Problem_40.java
+++ Problem_40.java
@@ -1,7 +1,9 @@
 import acm.program.GraphicsProgram;
 
 public class Problem_40 extends GraphicsProgram {
+	private static final int RADIUS = 50;
+	
 	public void run() {
-		
+		GOval oval = new GOval(2 * RADIUS, 2 * RADIUS);
 	}
 }
