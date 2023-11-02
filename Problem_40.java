6c8238976c3ae0f8c72aab0140ec09f553c6df29
S <s>
2023-11-02 23:45:30 +0400

    {"types":"C","runs":11}

--- Problem_40.java
+++ Problem_40.java
@@ -14,21 +14,21 @@ public class Problem_40 extends GraphicsProgram {
 		add(oval, xStart, yConst);
 		
 		
-		
-		while (xStart != xEnd) {
-			oval.setLocation(xStart, yConst);
-			pause(1);
-			xStart++;
-			
-			if (xStart == xEnd) {
-				xEnd = 0;
-				while (xStart != xEnd) {
-					oval.setLocation(xStart - 1, yConst);
-					pause(1);
-					xStart--;
+		while (true) {
+			while (xStart != xEnd) {
+				oval.setLocation(xStart, yConst);
+				pause(1);
+				xStart++;
+				
+				if (xStart == xEnd) {
+					xEnd = 0;
+					while (xStart != xEnd) {
+						oval.setLocation(xStart - 1, yConst);
+						pause(1);
+						xStart--;
+					}
 				}
 			}
 		}
-		
 	}
 }
